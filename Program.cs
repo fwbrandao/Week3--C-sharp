bfd60be7ee26863d1c4ae91e1cf32f3b5de16bb4
Fernando Brandao <fwbrandao@gmail.com>
2018-01-26 09:44:59 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace riddle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hey, i've got a riddle for you!!!");
+            Console.WriteLine("Why fish dont like basketball?");
+
+            string riddle = "they dont like the net";
+
+            string yourAnswer = Console.ReadLine().ToLower();
+
+            while (yourAnswer != riddle)
+            {
+                Console.WriteLine("Wrong, try again");
+                Console.ReadLine().ToLower();
+            }
+            if (yourAnswer == riddle)
+            {
+                Console.WriteLine("You've got it");
+            }
+
+
+
+        }
+    }
+}
+
