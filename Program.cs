c9604062504c507669b2c49f3521c59b8f8e7b60
Fernando Brandao <fwbrandao@gmail.com>
2018-01-26 09:29:04 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace dwarves
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            string[] sevenDwarves = { "Happy", "Doc", "Sleepy", "Sneezy", "Dopey", "Bashful", "Grumpy" };
+            Console.WriteLine("here are the seven dwarves: ");
+
+            for (int i = 0; i < sevenDwarves.Length; i++)
+            {
+                Console.WriteLine(sevenDwarves[i]);
+            }
+
+            Console.WriteLine(" The Food is ready:");
+
+            for (int i = 0; i < sevenDwarves.Length; i++)
+            {
+                Console.WriteLine("Heres you food " + sevenDwarves[i]);
+            }
+
+            Console.WriteLine("The Drink is ready:");
+
+            foreach (string dwarves in sevenDwarves)
+            {
+                Console.WriteLine("here is you drink " + dwarves);
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+           
