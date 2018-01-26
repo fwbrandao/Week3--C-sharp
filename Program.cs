3770e3c378170f0eea6c6b36add693e30cc19bf2
Fernando Brandao <fwbrandao@gmail.com>
2018-01-26 09:59:52 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace listOfBooks
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+
+
+            Console.WriteLine("Here are the books in or list. Press A to add book or X to exit.");
+
+            List<string> myBooks = new List<string>() { "Ready Player One", "The Girl on the Train" };
+
+            string bookToAdd = "";
+            string bookToRemove = "";
+            char userSelection = ' ';
+
+            do
+            {
+                Console.WriteLine("Press A to add book or X to exit.");
+                userSelection = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userSelection == 'a'){
+                    
+                    Console.WriteLine("please, tipe the name of your book.");
+                    bookToAdd = Convert.ToString(Console.ReadLine().ToLower());
+                    myBooks.Add(bookToAdd);
+                }
+
+
+                else{
+                    Console.WriteLine("here is your list");
+                }
+
+                Console.WriteLine(myBooks);
+
+            } while (userSelection != 'x' );
+
+              
+
+
+
+            }
+         }
+    }
+
